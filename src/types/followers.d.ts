0e98b8e80411e8aa8e0b309b263bed421a12a6ec
Andrey Ilyukhin <startopandrey@gmail.com>
2023-10-11 10:04:29 +0300

    follower type

--- src/types/followers.d.ts
+++ src/types/followers.d.ts
@@ -2,3 +2,21 @@ export type FollowersStackNavigatorParamList = {
   Followers: undefined;
   FollowerDetails: undefined;
 };
+export interface FollowersContextType {}
+
+export default interface Follower {
+  _id?: string;
+  birth_year: string;
+  eye_color?: string;
+  films?: string[];
+  gender: string;
+  hair_color?: string;
+  height?: string;
+  homeworld?: string;
+  mass?: string;
+  name: string;
+  skin_color?: string;
+  species?: string[];
+  starships?: string[];
+  vehicles?: string[];
+} // eslint-disable-line
