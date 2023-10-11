36e9c2a7d39b62337a1967c845104b0ed6557004
Andrey Ilyukhin <startopandrey@gmail.com>
2023-10-11 11:44:04 +0300

    mui paper provider

--- App.tsx
+++ App.tsx
@@ -8,13 +8,15 @@
 import React from 'react';
 import {ThemeProvider} from 'styled-components/native';
 import {Navigation} from './src/infrastructure/navigation/index.navigator';
-
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
 import {theme} from './src/infrastructure/theme';
 
 function App(): JSX.Element | null {
   return (
     <ThemeProvider theme={theme}>
-      <Navigation></Navigation>
+      <PaperProvider theme={DefaultTheme}>
+        <Navigation></Navigation>
+      </PaperProvider>
     </ThemeProvider>
   );
 }
